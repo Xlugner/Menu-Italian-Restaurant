561f4b51e587071b5681939cb5d56b845004ed54
Xlugner <madarakk31@gmail.com>
2025-08-29 01:34:53 -0400

    subiendo

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors:{
+				primary: '#157f71',
+				secondary: '#db1d00'
+			},
+			backgroundImage:{
+				'hero-pattern': 'url(/hero-pattern.jpg)'
+			} 
+		},
+	},
+	plugins: [],
+}
